610bc99a9cdfb1338ead47ab579c482060215c5f
Tu Nguyen <nvminhtu@gmail.com>
2019-08-22 07:29:24 +0700

    Vue translation

--- instagram-hashtag/pages/index.vue
+++ instagram-hashtag/pages/index.vue
@@ -9,19 +9,34 @@
         My instagram tools
       </h2>
       <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+      <TranslateOutput v-text="translatedText"></TranslateOutput>
     </div>
-    
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import TranslateForm from '~/components/TranslateForm.vue'
+import TranslateOutput from '~/components/TranslateOut.vue';
 
 export default {
-  components: {
-    Logo,
-    TranslateForm
+  name: 'app',
+  components:{
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return {
+      translatedText:''
+    }
+  },
+  methods: {
+    translateText:function(text, language){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170329T180255Z.3c18d2dc7b65d525.f23ed9a9efa992bded4ef96334e3c154f61d2dea&lang='+language+'&text='+text)
+      .then((response) => {
+        this.translatedText = response.body.text[0];
+      });
+    }
   }
 }
 </script>
